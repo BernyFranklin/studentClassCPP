81d0da20b96e406b1632aec36e5b367ae35854e5
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-30 13:35:44 -0700

    Created main, created header

---
+++ src/studentClassCPP/studentClassCPP/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * studentClassCPP Version#1.0
+ * Frank Bernal
+ * CIS 054
+ * Input
+ * Output
+ * 30 Mar 2022
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
