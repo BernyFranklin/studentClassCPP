ca30528a6ffac2b21e950c2b4665dadc4e3acb6a
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-30 15:04:15 -0700

    Tweaked display output

--- src/studentClassCPP/studentClassCPP/main.cpp
+++ src/studentClassCPP/studentClassCPP/main.cpp
@@ -8,19 +8,20 @@
  */
 
 #include <iostream>
-
+// Used for setw
+#include <iomanip>
 // Include the header file we made
 #include "Student.h"
 using namespace std;
 
 // Define an array of students
 Student CIS054[] = {
-    Student ("Joe Williams", 44536, 3.4),
-    Student ("Sally Washington", 55458, 3.7),
-    Student ("Fred MacIntosh", 66587, 2.9),
-    Student ("Jose De La Cruz", 67892, 4.0),
-    Student ("777 Dan McElroy", 77777, 4.0),
-    Student ("Thinh Nguyen", 73657, 3.6)
+    Student ("Joe Williams", 44536, "Nutrition", 3.4),
+    Student ("Sally Washington", 55458, "Accounting", 5.0),
+    Student ("Fred MacIntosh", 66587, "Computer Science", 2.9),
+    Student ("Jose De La Cruz", 67892, "Marketing", 4.0),
+    Student ("777 Dan McElroy", 77777, "Business Management", 4.0),
+    Student ("Thinh Nguyen", 736570, "Computer Science", 3.6)
 };   // End of array
 
 // Start Main
@@ -29,10 +30,16 @@ int main(int argc, const char * argv[]) {
     int numberOfStudents = sizeof(CIS054)/sizeof(Student);
     
     // Display the header line
+    cout << "            CIS 054 C/C++ Programming             " << endl;
+    cout << "==================================================" << endl;
+    cout << "  " << std::left << setw(7) << "ID#" << setw(22) << "Name" << "Major" << endl;
+    cout << "==================================================" << endl;
     
     // List of all the students in the course
     for (int i = 0; i < numberOfStudents; i++) {
-        cout << "  " << CIS054[i].getIdNumber() << "  " << CIS054[i].getName() << endl;
+        cout << std::left << "  " << setw(5) << setfill('0') << CIS054[i].getIdNumber() << "  "
+             << setw(20) << setfill(' ') << CIS054[i].getName() << "  "
+             << CIS054[i].getMajor() << endl;
     }   // End of loop
     
     cout << endl;   // Blank line between students names and grades
@@ -45,7 +52,8 @@ int main(int argc, const char * argv[]) {
     double average = total / numberOfStudents;    // Divide total by number of students
     
     // Print massage
-    cout << "  " << "The Average GPA of all the students is " << average << endl << endl;
+    cout << setiosflags(ios::fixed | ios::showpoint);   // Used to show decimal
+    cout << "  " << "The Average GPA of all the students is " << setprecision(2) << average << endl << endl;
     
     
         
